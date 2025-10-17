b11e360fc721d4154799d050dc6815258afcfc5b
GiovanniBBendetti <senaigiovannibenedetti@gmail.com>
2025-10-17 15:43:18 -0300

    Funcionários integrado

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+const port = 8080
+import funcionarioRotas from './routes/funcionarioRotas.js';
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/funcionarios', funcionarioRotas);
+
+app.get('/', (req,res) =>{
+res.status(200).json({mesagem : "API MusicHouse"})
+})
+
+
+app.use((req,res) =>{
+    res.status(404).json({mensagem: 'Rota não encontrada'})
+})
+
+app.listen(port, ()=>{
+    console.log(`Projeto sendo executado no http://locahost:${port}`)
+})
+
+
